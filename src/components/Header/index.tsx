3c280edf643100e41d4b5fde49e40f7ee6ff9825
Jun Joy <chojuneseo1@gmail.com>
2021-11-25 03:23:32 +0900

    [UPDATE] implemented header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,47 +1,59 @@
 import { FC } from "react";
 import * as S from "./styles";
-import { Logo } from "../../assets";
+import { Logo, DefaultProfile } from "../../assets";
 import { color } from "../../style";
 
 const Header: FC = (): JSX.Element => {
   return (
     <S.Wrapper>
       <S.HeaderContainer>
-      <S.LogoBox to="/">
-        <img src={Logo} />
-      </S.LogoBox>
-      <S.NavBox>
-        <S.NavList>
-          <S.NLink
-            style={({ isActive }) => ({
-              borderBottom: isActive ? `2px solid ${color.blue000}` : "none",
-            })}
-            to="/"
-          >
-            시각화
-          </S.NLink>
-        </S.NavList>
-        <S.NavList>
-          <S.NLink
-            to="/challenge"
-            style={({ isActive }) => ({
-              borderBottom: isActive ? `2px solid ${color.blue000}` : "none",
-            })}
-          >
-            첼린지
-          </S.NLink>
-        </S.NavList>
-        <S.NavList>
-          <S.NLink
-            to="/connection"
-            style={({ isActive }) => ({
-              borderBottom: isActive ? `2px solid ${color.blue000}` : "none",
-            })}
-          >
-            커넥
-          </S.NLink>
-        </S.NavList>
-      </S.NavBox>
+        <S.LogoBox to="/">
+          <img src={Logo} />
+        </S.LogoBox>
+        <S.NavBox>
+          <S.NavList>
+            <S.NLink
+              style={({ isActive }) => ({
+                borderBottom: isActive ? `2px solid ${color.blue000}` : "none",
+                marginBottom: isActive ? "0px" : "2px",
+              })}
+              to="/"
+            >
+              시각화
+            </S.NLink>
+          </S.NavList>
+          <S.NavList>
+            <S.NLink
+              to="/challenge"
+              style={({ isActive }) => ({
+                borderBottom: isActive ? `2px solid ${color.blue000}` : "none",
+                marginBottom: isActive ? "0px" : "2px",
+              })}
+            >
+              첼린지
+            </S.NLink>
+          </S.NavList>
+          <S.NavList>
+            <S.NLink
+              to="/connection"
+              style={({ isActive }) => ({
+                borderBottom: isActive ? `2px solid ${color.blue000}` : "none",
+                marginBottom: isActive ? "0px" : "2px",
+              })}
+            >
+              커넥
+            </S.NLink>
+          </S.NavList>
+          <S.NavList>
+            <S.ProfileBox to="/profile">
+              <img src={DefaultProfile} />
+              <S.ProfileName>
+                조준서
+                <span>님</span>
+              </S.ProfileName>
+            </S.ProfileBox>
+          </S.NavList>
+        </S.NavBox>
       </S.HeaderContainer>
     </S.Wrapper>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,46 +1,74 @@
 import styled from "@emotion/styled";
-import { NavLink, Link } from 'react-router-dom';
-import { color } from '../../style';
+import { NavLink, Link } from "react-router-dom";
+import { color } from "../../style";
 
 export const LogoBox = styled(Link)`
-    text-decoration: none;
-    outline: none;
+  text-decoration: none;
+  outline: none;
+  & img {
+    height: 24px;
+  }
 `;
 
 export const HeaderContainer = styled.div`
-    width: 1440px;
-    height: 100%;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  width: 80%;
+  min-width: 750px;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const Wrapper = styled.div`
-    height: 80px;
-    border-bottom: 1px solid ${color.gray100};
-    & img {
-        height: 40px;
-    }
+  height: 80px;
+  border-bottom: 1px solid ${color.gray100};
 `;
 
 export const NavBox = styled.ul`
-    display: flex;
-    align-items: center;
-    height: 100%;
-    gap: 45px;
+  display: flex;
+  align-items: center;
+  height: 100%;
+  gap: 50px;
 `;
 
 export const NavList = styled.li`
-    display: flex;
-    align-items: center;
-    font-size: 18px;
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+  height: 100%;
 `;
 
-
 export const NLink = styled(NavLink)`
-    color: ${color.black000};
-    text-decoration: none;
-    outline: none;
-    height: 22px;
+  color: ${color.black000};
+  text-decoration: none;
+  outline: none;
+  height: 22px;
+  display: flex;
+  align-items: center;
+`;
+
+export const ProfileBox = styled(Link)`
+  display: flex;
+  text-decoration: none;
+  outline: none;
+  justify-content: space-between;
+  color: ${color.black000};
+  align-items: center;
+  width: 100px;
+  margin-bottom: 2px;
+  & img {
+    width: 25px;
+    height: 25px;
+  }
+`;
+
+export const ProfileName = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  display: flex;
+  gap: 1px;
+  & span {
+    font-weight: 500;
+  }
 `;
