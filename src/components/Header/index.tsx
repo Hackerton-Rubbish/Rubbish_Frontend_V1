c75dfca432ce0ce889fe4f257b217263a1ec5214
Jun Joy <chojuneseo1@gmail.com>
2021-11-25 00:36:29 +0900

    [UPDATE] edit styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,16 @@
 import { FC } from "react";
+import * as S from "./styles";
+import { Logo } from "../../assets";
+import { NavLink } from "react-router-dom";
 
 const Header: FC = (): JSX.Element => {
   return (
-    <>
-      <div>Header</div>
-    </>
+    <S.Wrapper>
+      <img src={Logo} />
+      <S.NavBox>
+        <NavLink to="/connection" />
+      </S.NavBox>
+    </S.Wrapper>
   );
 };
 
