f33cd7faa1cfdef02cf7e19d5bfe35a12e1c23fc
Jun Joy <chojuneseo1@gmail.com>
2021-11-25 02:49:56 +0900

    [CREATE] navllink active style implemented

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,48 @@
 import { FC } from "react";
 import * as S from "./styles";
 import { Logo } from "../../assets";
-import { NavLink } from "react-router-dom";
+import { color } from "../../style";
 
 const Header: FC = (): JSX.Element => {
   return (
     <S.Wrapper>
-      <img src={Logo} />
+      <S.HeaderContainer>
+      <S.LogoBox to="/">
+        <img src={Logo} />
+      </S.LogoBox>
       <S.NavBox>
-        <NavLink to="/connection" />
+        <S.NavList>
+          <S.NLink
+            style={({ isActive }) => ({
+              borderBottom: isActive ? `2px solid ${color.blue000}` : "none",
+            })}
+            to="/"
+          >
+            시각화
+          </S.NLink>
+        </S.NavList>
+        <S.NavList>
+          <S.NLink
+            to="/challenge"
+            style={({ isActive }) => ({
+              borderBottom: isActive ? `2px solid ${color.blue000}` : "none",
+            })}
+          >
+            첼린지
+          </S.NLink>
+        </S.NavList>
+        <S.NavList>
+          <S.NLink
+            to="/connection"
+            style={({ isActive }) => ({
+              borderBottom: isActive ? `2px solid ${color.blue000}` : "none",
+            })}
+          >
+            커넥
+          </S.NLink>
+        </S.NavList>
       </S.NavBox>
+      </S.HeaderContainer>
     </S.Wrapper>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,14 +1,24 @@
 import styled from "@emotion/styled";
-import { color } from "../../style";
+import { NavLink, Link } from 'react-router-dom';
+import { color } from '../../style';
 
-export const Wrapper = styled.div`
+export const LogoBox = styled(Link)`
+    text-decoration: none;
+    outline: none;
+`;
+
+export const HeaderContainer = styled.div`
     width: 1440px;
-    height: 80px;
+    height: 100%;
     margin: 0 auto;
     display: flex;
     justify-content: space-between;
     align-items: center;
+`;
 
+export const Wrapper = styled.div`
+    height: 80px;
+    border-bottom: 1px solid ${color.gray100};
     & img {
         height: 40px;
     }
@@ -21,4 +31,16 @@ export const NavBox = styled.ul`
     gap: 45px;
 `;
 
-// export const Nav
+export const NavList = styled.li`
+    display: flex;
+    align-items: center;
+    font-size: 18px;
+`;
+
+
+export const NLink = styled(NavLink)`
+    color: ${color.black000};
+    text-decoration: none;
+    outline: none;
+    height: 22px;
+`;
