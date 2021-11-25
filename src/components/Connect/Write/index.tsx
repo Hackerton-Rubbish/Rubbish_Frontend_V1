ec00f1aaeaca2edbbdeee6bc4b17ed6056e01828
Jun Joy <chojuneseo1@gmail.com>
2021-11-25 18:19:13 +0900

    [UPDATE] connection implementation

---
+++ src/components/Connect/Write/index.tsx
@@ -0,0 +1,46 @@
+import { FC, useState } from "react";
+import * as S from "./styles";
+import { Flag, ImageInput } from "../../../assets";
+
+const Write: FC = (): JSX.Element => {
+  const [selectedFile, setSelectedFile] = useState("");
+  const [fileImage, setFileImage] = useState<any>();
+
+  const handFileChange = (e: any) => {
+    setSelectedFile(e.target.files[0]);
+    setFileImage(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const handleFileUpload = () => {
+    //     const formData = new FormData();
+    //     formData.append("userfile", selectedFile, selectedFile.name);
+  };
+
+  return (
+    <S.Wrapper>
+      <S.ImageInputContainer>
+        <S.PreviewImage>
+            <img src={fileImage}/>
+        </S.PreviewImage>
+        <input type="file" accept="image/*" onChange={handFileChange}/>
+      </S.ImageInputContainer>
+      <S.InputBoxContainer>
+        <S.InputBox>
+            <S.InputTitle>종류</S.InputTitle>
+            <input placeholder="종류를 입력하세요"/>
+        </S.InputBox>
+        <S.InputBox>
+            <S.InputTitle>수량</S.InputTitle>
+            <input placeholder="몇개를 가지고 있으신가요"/>
+        </S.InputBox>
+        <S.InputBox>
+            <S.InputTitle>지역</S.InputTitle>
+            <input placeholder="거래하시는 지역이 어디인가요"/>
+        </S.InputBox>
+        <S.ShareButton onClick={handleFileUpload}>나눔하기</S.ShareButton>
+      </S.InputBoxContainer>
+    </S.Wrapper>
+  );
+};
+
+export default Write;
---
+++ src/components/Connect/Write/styles.ts
@@ -0,0 +1,71 @@
+import styled from "@emotion/styled";
+import { ImageInput } from "../../../assets";
+import { color } from "../../../style";
+
+export const Wrapper = styled.div`
+  width: 800px;
+  margin: 0 auto;
+  height: auto;
+  display: flex;
+  align-items: center;
+  gap: 60px;
+  height: 80vh;
+`;
+
+export const ImageInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const PreviewImage = styled.div`
+  min-width: 350px;
+  min-height: 250px;
+  border: 1px solid black;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const InputBoxContainer = styled.div`
+  height: 300px;
+  display: grid;
+  grid-template-rows: repeat(4, 1fr);
+  place-items: center;
+  gap: 20px;
+  input {
+    outline: none;
+    text-decoration: none;
+    border: none;
+    border-bottom: 1px solid #949494;
+    width: 280px;
+    margin-left: 20px;
+    font-size: 16px;
+  }
+`;
+
+export const InputBox = styled.div`
+  display: flex;
+`;
+
+export const InputTitle = styled.div`
+  font-size: 24px;
+  width: 45px;
+`;
+
+export const ShareButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: ${color.blue000};
+  height: 50px;
+  color: ${color.gray000};
+  border-radius: 5px;
+  cursor: pointer;
+`;
